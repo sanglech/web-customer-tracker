5b8d2577b54cf829c6f8b6eea78fbb8c75d12ca2
Christian <christian@sanglech.com>
2021-04-15 12:08:40 +0900

    -Initial Setup for AOP in CRM app
    -Added aop declarations in spring xml
    - changed the contet root to be we-customer-tracker-aop
    -Started making logging aspect

---
+++ src/main/java/com/christian/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,22 @@
+package com.christian.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+	
+	//setup logger
+	
+	private Logger myLogger= Logger.getLogger(getClass().getName());	
+	//setup pointcut declarations
+	
+	
+	//setup before advice
+	
+	//setup after returning advice
+}
