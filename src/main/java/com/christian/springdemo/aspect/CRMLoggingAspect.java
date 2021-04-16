6076213ccba8ec68d5c40a0bf0c9c84bb5fd51d4
Christian <christian@sanglech.com>
2021-04-16 13:03:30 +0900

    Added @AfterReturning advice

--- src/main/java/com/christian/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/christian/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.christian.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -55,4 +56,27 @@ public class CRMLoggingAspect {
 		
 	}
 	//setup after returning advice
+	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		//display the method we are calling
+		
+		String theMethod= theJoinPoint.getSignature().toShortString();
+		myLogger.info("--------> in @AfterReturning: from method "+ theMethod);
+		
+		
+		//display the data being returned
+		myLogger.info("-------> the result: "+theResult);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
