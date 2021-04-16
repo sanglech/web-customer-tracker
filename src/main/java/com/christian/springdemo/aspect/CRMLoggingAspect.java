d42b5ba989602142be20f7cff9cec0a041d082cc
Christian <christian@sanglech.com>
2021-04-16 12:57:31 +0900

    Setup @before advice to log for all packages

--- src/main/java/com/christian/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/christian/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,10 @@ package com.christian.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 
@@ -15,8 +18,41 @@ public class CRMLoggingAspect {
 	private Logger myLogger= Logger.getLogger(getClass().getName());	
 	//setup pointcut declarations
 	
+	@Pointcut("execution(* com.christian.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.christian.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	
+	@Pointcut("execution(* com.christian.springdemo.DAO.*.*(..))")
+	private void forDAOPackage() {}
+	
+	
+	
+	@Pointcut("forControllerPackage() ||forServicePackage() ||forDAOPackage() ")
+	private void  forAppFlow(){}
+	
 	
 	//setup before advice
-	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		//display the method we are calling
+		
+		String theMethod= theJoinPoint.getSignature().toShortString();
+		myLogger.info("=========> in @Before: calling method "+ theMethod);
+		
+		//display the args
+		
+		//get the arguements
+		Object [] args= theJoinPoint.getArgs();
+		
+		//loop through and display the arguements
+		for(Object arg: args) {
+			myLogger.info("=======> arguement: "+arg);
+		}
+		
+		
+	}
 	//setup after returning advice
 }
